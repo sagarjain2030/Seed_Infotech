6e94154df1056c551660248064cbd003d6e3328b
Sagar Jain <sagarjain2030@gmail.com>
2023-11-01 06:30:02 +0530

    Lab00_Arrays III
    Lab 03 is done

--- AdvancedCProgrammingBook/Ch00_ArrayRevisited/lab03.c
+++ AdvancedCProgrammingBook/Ch00_ArrayRevisited/lab03.c
@@ -8,53 +8,82 @@
 #define size 5
 
 void AcceptArray(int* arr);
+void PrintArray(int* arr);
 void FindMaxMin(int* arr,int* max,int* min);
-int* MultiplyByn(int* arr,int n);
+void MultiplyByn(int* arr,int sizeArray, int n, int* newArr);
 int main(void)
 {
     int arr[size];
     AcceptArray(arr);
-    int max = arr[0];
-    int min = arr[0];
+    PrintArray(arr);
+    int max = arr[size - 1];
+    int min = arr[size - 1];
     FindMaxMin(arr,&max,&min);
     printf("Maximum element in array is %d\n",max);
     printf("Minimum element in array is %d\n",min);
-    int * new_arr = MultiplyByn(arr,5);
+    int new_arr[size]; 
+    MultiplyByn(arr,size,5,new_arr);
+    PrintArray(new_arr);
     return 0;
 }
 
 void AcceptArray(int *arr)
 {
-    printf("Enter %d integers for the array \n : ",size);
+    printf("Enter %d integers for the array  : \n",size);
     for(int  i = 0 ;i < size; i++)
     {
         scanf("%d", &arr[i]);
     }
 }
 
-void FindMaxMin(int* arr,int* max, int * min)
+void PrintArray(int *arr)
 {
-    max = arr[0];
-    min = arr[0];
+    printf("The array is : ");
+    for(int i = 0 ; i < size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void FindMaxMin(int* arr,int* max, int* min)
+{
+    *max = arr[0];
+    *min = arr[0];
     for(int i = 1; i < size; i++)
     {
         if(arr[i] > *max)
-          *max = arr[i];
+            *max = arr[i];
         if(arr[i] < *min)
-          *min = arr[i];  
+            *min = arr[i];
     }
 }
 
-int* MultiplyByn(int* arr,int n)
+void MultiplyByn(int* arr,int sizeArray, int n, int* newArr)
 {
     if(n < 0)
     {
         printf("Invalid value of n. Returning original array.\n");
-        return &arr;
+        for(int i= 0 ; i < sizeArray; i++)
+            newArr[i] = arr[i];
     }
-    int new_arr[size];
-    for(int i = 0 ; i < size; i++)
-        new_arr[i] = arr[i] * n;
-
-    return new_arr;
+    else
+    {
+        for(int i= 0 ; i < sizeArray; i++)
+            newArr[i] = arr[i] * n;
+    }
 }
+
+// Output :
+/*
+Enter 5 integers for the array  : 
+39
+67
+48
+57
+74
+The array is : 39 67 48 57 74 
+Maximum element in array is 74
+Minimum element in array is 39
+The array is : 195 335 240 285 370
+*/
